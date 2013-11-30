c22ad0fef84794191adecb7ed6146f0f7661b89c
fsp <fsp261@gmail.com>
2013-11-30 16:34:12 +0800

    deployment.reverse_url_fix

---
+++ deployment/fix_url_behind_reverse.py
@@ -0,0 +1,60 @@
+# -*- coding: utf-8 -*-
+"""
+    deployment.reverse_url_fix
+    ~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+    Fixing SCRIPT_NAME/url_scheme when behind reverse proxy
+    http://flask.pocoo.org/snippets/35/
+"""
+
+import os
+import sys
+sys.path.insert(0, os.path.dirname(os.path.abspath(os.path.dirname(__file__))))
+
+from flask import request, Response
+
+from app import app
+
+
+class ReverseProxied(object):
+    '''Wrap the application in this middleware and configure the 
+    front-end server to add these headers, to let you quietly bind 
+    this to a URL other than / and to an HTTP scheme that is 
+    different than what is used locally.
+
+    In nginx:
+    location /myprefix {
+        proxy_pass http://192.168.0.1:5001;
+        proxy_set_header Host $host;
+        proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
+        proxy_set_header X-Scheme $scheme;
+        proxy_set_header X-Script-Name /myprefix;
+        }
+
+    :param app: the WSGI application
+    '''
+    def __init__(self, app):
+        self.app = app
+
+    def __call__(self, environ, start_response):
+        script_name = environ.get('HTTP_X_SCRIPT_NAME', '')
+        if script_name:
+            environ['SCRIPT_NAME'] = script_name
+            path_info = environ['PATH_INFO']
+            if path_info.startswith(script_name):
+                environ['PATH_INFO'] = path_info[len(script_name):]
+
+        scheme = environ.get('HTTP_X_SCHEME', '')
+        if scheme:
+            environ['wsgi.url_scheme'] = scheme
+        return self.app(environ, start_response)
+
+
+@app.route('/')
+def index():
+    return 'index'
+
+
+if __name__ == "__main__":
+    app.wsgi_app = ReverseProxied(app.wsgi_app)
+    app.run()
