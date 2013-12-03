221ceca0baa893b32d3f89535aafd2eec570a5d4
fsp <fsp261@gmail.com>
2013-12-03 14:57:12 +0800

    appstructure.json_oriented

---
+++ appstructure/json_oriented/__init__.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+"""
+    appstructure.json_oriented
+    ~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+    Specialized JSON-oriented Flask App
+    http://flask.pocoo.org/snippets/83/
+"""
+from flask import Flask, jsonify
+from werkzeug.exceptions import default_exceptions
+from werkzeug.exceptions import HTTPException
+
+__all__ = ['make_json_app']
+
+def make_json_app(import_name, **kwargs):
+    """
+    Creates a JSON-oriented Flask app.
+
+    All error responses that you don't specifically
+    manage yourself will have application/json content
+    type, and will contain JSON like this (just an example):
+
+    { "message": "405: Method Not Allowed" }
+    """
+    def make_json_error(ex):
+        response = jsonify(message=str(ex))
+        response.status_code = (ex.code
+                                if isinstance(ex, HTTPException)
+                                else 500)
+        return response
+
+    app = Flask(import_name, **kwargs)
+
+    for code in default_exceptions.iterkeys():
+        app.error_handler_spec[None][code] = make_json_error
+
+    return app
