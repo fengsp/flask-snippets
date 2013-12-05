a5905ad47ff30e6afc0cc4183ef671182ee15606
fsp <fsp261@gmail.com>
2013-12-05 15:00:56 +0800

    templatetricks.timesince_filter

---
+++ templatetricks/timesince_filter.py
@@ -0,0 +1,43 @@
+# -*- coding: utf-8 -*-
+"""
+    templatetricks.timesince_filter
+    ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+    timesince filter
+    http://flask.pocoo.org/snippets/33/
+"""
+
+import os
+import sys
+sys.path.insert(0, os.path.dirname(os.path.abspath(os.path.dirname(__file__))))
+from datetime import datetime
+
+from app import app
+
+
+@app.template_filter()
+def timesince(dt, default="just now"):
+    """
+    Returns string representing "time since" e.g.
+    3 days ago, 5 hours ago etc.
+    """
+
+    now = datetime.utcnow()
+    diff = now - dt
+    
+    periods = (
+        (diff.days / 365, "year", "years"),
+        (diff.days / 30, "month", "months"),
+        (diff.days / 7, "week", "weeks"),
+        (diff.days, "day", "days"),
+        (diff.seconds / 3600, "hour", "hours"),
+        (diff.seconds / 60, "minute", "minutes"),
+        (diff.seconds, "second", "seconds"),
+    )
+
+    for period, singular, plural in periods:
+        
+        if period:
+            return "%d %s ago" % (period, singular if period == 1 else plural)
+
+    return default
