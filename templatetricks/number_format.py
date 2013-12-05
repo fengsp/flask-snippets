a0a0d30dc5c4aec35ebdd9641318bdb45375545f
fsp <fsp261@gmail.com>
2013-12-05 14:59:26 +0800

    templatetricks.number_format

---
+++ templatetricks/number_format.py
@@ -0,0 +1,43 @@
+# -*- coding: utf-8 -*-
+"""
+    templatetricks.number_format
+    ~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+    number_format filter
+    http://flask.pocoo.org/snippets/29/
+"""
+
+import os
+import sys
+sys.path.insert(0, os.path.dirname(os.path.abspath(os.path.dirname(__file__))))
+
+from app import app
+
+
+@app.template_filter()
+def number_format(value, tsep=',', dsep='.'):
+    s = unicode(value)
+    cnt = 0
+    numchars = dsep + '0123456789'
+    ls = len(s)
+    while cnt < ls and s[cnt] not in numchars:
+        cnt += 1
+
+    lhs = s[:cnt]
+    s = s[cnt:]
+    if not dsep:
+        cnt = -1
+    else:
+        cnt = s.rfind(dsep)
+    if cnt > 0:
+        rhs = dsep + s[cnt+1:]
+        s = s[:cnt]
+    else:
+        rhs = ''
+
+    splt = ''
+    while s != '':
+        splt = s[-3:] + tsep + splt
+        s = s[:-3]
+
+    return lhs + splt[:-1] + rhs
