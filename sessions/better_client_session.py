92f2588226b0a2053a5eef72512e881f387d38a3
fsp <fsp261@gmail.com>
2013-12-05 15:13:37 +0800

    sessions.better_client_session

---
+++ sessions/better_client_session.py
@@ -0,0 +1,68 @@
+# -*- coding: utf-8 -*-
+"""
+    sessions.better_client_session
+    ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+    Better Client-side sessions
+    http://flask.pocoo.org/snippets/51/
+"""
+
+import os
+import sys
+sys.path.insert(0, os.path.dirname(os.path.abspath(os.path.dirname(__file__))))
+
+from werkzeug.datastructures import CallbackDict
+from flask.sessions import SessionInterface, SessionMixin
+from itsdangerous import URLSafeTimedSerializer, BadSignature
+
+from app import app
+
+
+class ItsdangerousSession(CallbackDict, SessionMixin):
+
+    def __init__(self, initial=None):
+        def on_update(self):
+            self.modified = True
+        CallbackDict.__init__(self, initial, on_update)
+        self.modified = False
+
+
+class ItsdangerousSessionInterface(SessionInterface):
+    salt = 'cookie-session'
+    session_class = ItsdangerousSession
+
+    def get_serializer(self, app):
+        if not app.secret_key:
+            return None
+        return URLSafeTimedSerializer(app.secret_key, 
+                                      salt=self.salt)
+
+    def open_session(self, app, request):
+        s = self.get_serializer(app)
+        if s is None:
+            return None
+        val = request.cookies.get(app.session_cookie_name)
+        if not val:
+            return self.session_class()
+        max_age = app.permanent_session_lifetime.total_seconds()
+        try:
+            data = s.loads(val, max_age=max_age)
+            return self.session_class(data)
+        except BadSignature:
+            return self.session_class()
+
+    def save_session(self, app, session, response):
+        domain = self.get_cookie_domain(app)
+        if not session:
+            if session.modified:
+                response.delete_cookie(app.session_cookie_name,
+                                   domain=domain)
+            return
+        expires = self.get_expiration_time(app, session)
+        val = self.get_serializer(app).dumps(dict(session))
+        response.set_cookie(app.session_cookie_name, val,
+                            expires=expires, httponly=True,
+                            domain=domain)
+
+
+app.session_interface = ItsdangerousSessionInterface()
